3c7913748002fa944312e7a6c6cbc9b21c437e36
0xtxbi <techiejossy@gmail.com>
2025-10-22 14:43:08 +0100

    improved amount input section

--- packages/ui/src/components/widgets/TokenWidget/AmountSectionHeader.tsx
+++ packages/ui/src/components/widgets/TokenWidget/AmountSectionHeader.tsx
@@ -1,24 +1,37 @@
 import { type FC } from 'react'
-import { Flex, Text } from '../../primitives/index.js'
-import { SlippageButton } from '../../primitives/SlippageButton.js'
+import { Flex, Text, Box } from '../../primitives/index.js'
+import { SlippageToleranceConfig } from '../../common/SlippageToleranceConfig.js'
 
 type AmountSectionHeaderProps = {
   label: string
   slippageTolerance?: string
+  onSlippageToleranceChange?: (value: string | undefined) => void
+  onAnalyticEvent?: (eventName: string, data?: any) => void
   onOpenSlippageConfig?: () => void
 }
 
 const AmountSectionHeader: FC<AmountSectionHeaderProps> = ({
   label,
   slippageTolerance,
+  onSlippageToleranceChange,
+  onAnalyticEvent,
   onOpenSlippageConfig
 }) => (
-  <Flex align="center" justify="between" css={{ gap: '2', width: '100%' }}>
-    <Text style="subtitle2">{label}</Text>
-    <SlippageButton
-      slippageTolerance={slippageTolerance}
-      onOpenSlippageConfig={onOpenSlippageConfig}
-    />
+  <Flex direction="column" css={{ gap: '0', width: '100%' }}>
+    <Flex align="center" justify="between" css={{ gap: '2', width: '100%' }}>
+      <Text style="subtitle2">{label}</Text>
+      <Box css={{ position: 'relative', zIndex: 1 }}>
+        <SlippageToleranceConfig
+          variant="inline"
+          label="Slippage"
+          showGearIcon={false}
+          currentSlippageTolerance={slippageTolerance}
+          setSlippageTolerance={onSlippageToleranceChange ?? (() => {})}
+          onAnalyticEvent={onAnalyticEvent}
+          onOpenSlippageConfig={onOpenSlippageConfig}
+        />
+      </Box>
+    </Flex>
   </Flex>
 )
 
