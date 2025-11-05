9798109e3e76d0f08aacf1be91deacb7a63e770f
0xtxbi <techiejossy@gmail.com>
2025-11-05 19:16:48 +0100

    proper exports

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -12,8 +12,6 @@ export { default as useRelayClient } from './hooks/useRelayClient.js'
 //widgets
 export { default as SwapWidget } from './components/widgets/SwapWidget/index.js'
 export type { SwapWidgetProps } from './components/widgets/SwapWidget/index.js'
-export { TokenWidget } from './components/widgets/TokenWidget/index.js'
-export type { TokenWidgetProps } from './components/widgets/TokenWidget/index.js'
 
 //components
 export { default as TokenSelector } from './components/common/TokenSelector/TokenSelector.js'
