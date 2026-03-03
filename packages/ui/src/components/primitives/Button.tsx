fcd4bc75b8617b7b032625b3cf17990ef63aa1ed
Ted Palmer <etpalmer17@gmail.com>
2026-03-03 14:27:55 -0500

    Add back but down scale on press

--- packages/ui/src/components/primitives/Button.tsx
+++ packages/ui/src/components/primitives/Button.tsx
@@ -8,8 +8,10 @@ const buttonVariants = cva(
     'relay-transition-all relay-duration-200 relay-ease-out relay-select-none',
     'relay-gap-2 relay-inline-flex relay-items-center relay-leading-[20px]',
     'focus-visible:relay-ring-2 focus-visible:relay-ring-[var(--relay-colors-focus-color)] focus-visible:relay-ring-offset-2',
+    'active:relay-scale-[0.98]',
     'disabled:relay-cursor-not-allowed disabled:relay-bg-[var(--relay-colors-button-disabled-background)] disabled:relay-text-[color:var(--relay-colors-button-disabled-color)]',
-    'disabled:hover:relay-bg-[var(--relay-colors-button-disabled-background)] disabled:hover:relay-text-[color:var(--relay-colors-button-disabled-color)]'
+    'disabled:hover:relay-bg-[var(--relay-colors-button-disabled-background)] disabled:hover:relay-text-[color:var(--relay-colors-button-disabled-color)]',
+    'disabled:active:relay-scale-100'
   ].join(' '),
   {
     variants: {
