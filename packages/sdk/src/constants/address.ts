ea91146b7cd2e6ec45f4a577155a2335cca42a25
Ted Palmer <ted@unevenlabs.com>
2026-06-22 11:38:55 -0400

    fix formatting

--- packages/sdk/src/constants/address.ts
+++ packages/sdk/src/constants/address.ts
@@ -1,39 +1,39 @@
-import type { ChainVM } from "../types/RelayChain.js";
+import type { ChainVM } from '../types/RelayChain.js'
 
 export const evmDeadAddress =
-  "0x000000000000000000000000000000000000dead" as const;
+  '0x000000000000000000000000000000000000dead' as const
 export const solDeadAddress =
-  "CbKGgVKLJFb8bBrf58DnAkdryX6ubewVytn7X957YwNr" as const;
+  'CbKGgVKLJFb8bBrf58DnAkdryX6ubewVytn7X957YwNr' as const
 export const eclipseDeadAddress =
-  "CrfbABN2sSvmoZLu9eDDfXpaC2nHg42R7AXbHs9eg4S9" as const;
-export const bitcoinDeadAddress = "bc1q4vxn43l44h30nkluqfxd9eckf45vr2awz38lwa";
-export const tronDeadAddress = "THa7BwoPfacfiELa63pbmm3g5PGKYmtJyt";
-export const zeroDeadAddress = "0x00000000000000000000000000000000000dead0";
+  'CrfbABN2sSvmoZLu9eDDfXpaC2nHg42R7AXbHs9eg4S9' as const
+export const bitcoinDeadAddress = 'bc1q4vxn43l44h30nkluqfxd9eckf45vr2awz38lwa'
+export const tronDeadAddress = 'THa7BwoPfacfiELa63pbmm3g5PGKYmtJyt'
+export const zeroDeadAddress = '0x00000000000000000000000000000000000dead0'
 export const tonDeadAddress =
-  "EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADerZ0z" as const;
+  'EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADerZ0z' as const
 
-const eclipseId = 9286185;
-const zeroChainId = 543210;
+const eclipseId = 9286185
+const zeroChainId = 543210
 
 export const getDeadAddress = (vmType?: ChainVM, chainId?: number) => {
-  if (vmType === "svm") {
-    return chainId === eclipseId ? eclipseDeadAddress : solDeadAddress;
-  } else if (vmType === "bvm") {
-    return bitcoinDeadAddress;
+  if (vmType === 'svm') {
+    return chainId === eclipseId ? eclipseDeadAddress : solDeadAddress
+  } else if (vmType === 'bvm') {
+    return bitcoinDeadAddress
   } else if (chainId === zeroChainId) {
-    return zeroDeadAddress;
-  } else if (vmType === "tvm") {
-    return tronDeadAddress;
-  } else if (vmType === "tonvm") {
-    return tonDeadAddress;
+    return zeroDeadAddress
+  } else if (vmType === 'tvm') {
+    return tronDeadAddress
+  } else if (vmType === 'tonvm') {
+    return tonDeadAddress
   } else {
-    return evmDeadAddress;
+    return evmDeadAddress
   }
-};
+}
 
 export const isDeadAddress = (address?: string) => {
   if (!address) {
-    return false;
+    return false
   }
 
   if (
@@ -43,8 +43,8 @@ export const isDeadAddress = (address?: string) => {
     address === evmDeadAddress ||
     address === tonDeadAddress
   ) {
-    return true;
+    return true
   }
 
-  return false;
-};
+  return false
+}
